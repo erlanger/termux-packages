801cdec6592b1e3ed489b6f45b7b7434dbfbdc9c
erlanger <erlanger@please_use_issues>
2018-12-03 18:50:04 -0500

    fix ca-certs and tmp directory

--- packages/swi-prolog/release-build.sh
+++ packages/swi-prolog/release-build.sh
@@ -1,26 +1,28 @@
                            ########################################
                            #          Set variables               #
                            ########################################
+_TMP_DIR=${TERMUX_PREFIX}/../../cache
 TERMUX_PKG_HOMEPAGE=https://swi-prolog.org/
 TERMUX_PKG_DESCRIPTION="Most popular and complete prolog implementation"
-TERMUX_PKG_VERSION=7.7.22
-TERMUX_PKG_REVISION=1
-TERMUX_PKG_SHA256="55096d6bc1d70463b3cf1148e43e8953733ab30ca06ba3f467b562762ed4cb4c"
+TERMUX_PKG_VERSION=7.7.23
+TERMUX_PKG_REVISION=3
+TERMUX_PKG_SHA256="79531c2604fbf32c32b35df4784ea4cf4a0f675ab68b041bf4ff6e50bcd38309"
 TERMUX_PKG_SRCURL=http://www.swi-prolog.org/download/devel/src/swipl-${TERMUX_PKG_VERSION}.tar.gz
 
-TERMUX_PKG_DEPENDS="readline, libgmp, libcrypt, pcre, libarchive, libyaml, libjpeg-turbo"
+TERMUX_PKG_DEPENDS="readline, libgmp, libcrypt, pcre, libarchive, libyaml, libjpeg-turbo, ossp-uuid"
 
 TERMUX_PKG_EXTRA_CONFIGURE_ARGS="
 -DINSTALL_DOCUMENTATION=OFF
 -DUSE_GMP=ON
 -DSWIPL_NATIVE_FRIEND=${TERMUX_PKG_HOSTBUILD_DIR}
 -DUNIX_SHELL=${TERMUX_PREFIX}/bin/sh
--DSWIPL_TMP_DIR=${TERMUX_PREFIX}/lib/swipl/tmp
+-DSWIPL_TMP_DIR=${_TMP_DIR}
 -DSWIPL_INSTALL_IN_LIB=ON
 -DSWIPL_PACKAGES_BDB=OFF
 -DSWIPL_PACKAGES_ODBC=OFF
 -DSWIPL_PACKAGES_QT=OFF
 -DSWIPL_PACKAGES_X=OFF
+-DSYSTEM_CACERT_FILENAME=${TERMUX_PREFIX}/etc/tls/cert.pem
 -C${TERMUX_PKG_BUILDER_DIR}/TryRunResults.cmake"
 
 #-DCMAKE_CROSSCOMPILING_EMULATOR=qemu-arm-static"
@@ -75,7 +77,7 @@ termux_step_host_build () {
                            ########################################
 #Executed after make install
 termux_step_post_make_install () {
-        mkdir -p ${TERMUX_PREFIX}/lib/swipl/tmp
+        mkdir -p ${_TMP_DIR}
 }
 
 
